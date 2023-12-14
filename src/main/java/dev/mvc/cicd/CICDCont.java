e94ad25361336b542de3103c841d508573007093
JSYANG0126 <didwnstjr23@naver.com>
2023-12-14 15:28:58 +0900

    CICD 2차 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -10,10 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class CICDCont {
   // http://localhost:9091/cicd?sw=-1
-  // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=1
+  // http://localhost:9091/cicd?sw=0
+  // http://localhost:9091/cicd?sw=2
   // http://localhost:9091/cicd?sw=3
-  // http://43.200.109.211:9091/cicd?sw=3
+  // http://3.34.236.207:9091/cicd?sw=3
   @GetMapping("/cicd")
   public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
     String msg = "";
@@ -31,7 +32,10 @@ public class CICDCont {
       msg = "/cide workflow 테스트";
       log.info(msg);
     } else if (sw == 3) {
-      msg = "/cide Githut Action + EC2 최종 테스트";
+      msg = "/cicd Github action + EC2 최종 테스트 1";
+      log.info(msg);
+    } else if (sw == 4) {
+      msg = "/cicd Github action + EC2 최종 테스트 2";
       log.info(msg);
     }
     
