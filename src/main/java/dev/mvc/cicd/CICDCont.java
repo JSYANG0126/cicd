9cb6c8c476a26e8a6c7c439da7b852494d6621bf
JSYANG0126 <didwnstjr23@naver.com>
2023-12-14 15:17:02 +0900

    CICD 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -12,7 +12,8 @@ public class CICDCont {
   // http://localhost:9091/cicd?sw=-1
   // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=1
-  // http://localhost:9091/cicd?sw=2
+  // http://localhost:9091/cicd?sw=3
+  // http://43.200.109.211:9091/cicd?sw=3
   @GetMapping("/cicd")
   public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
     String msg = "";
@@ -28,6 +29,9 @@ public class CICDCont {
       log.info(msg);
     } else if (sw == 2) {
       msg = "/cide workflow 테스트";
+      log.info(msg);
+    } else if (sw == 3) {
+      msg = "/cide Githut Action + EC2 최종 테스트";
       log.info(msg);
     }
     
